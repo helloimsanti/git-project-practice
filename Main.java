c5bf93b475c6a453bfd4009ce69da0ba35eaa757
helloimsanti <santyalvareza10@gmail.com>
2026-09-21 16:04:25 -0400

    Created account class

--- Main.java
+++ Main.java
@@ -1,1 +1,11 @@
+class Account {
+    private String firstName;
+    private String lastName;
+    private double balance;
 
+    public Account(String firstName, String lastName, double balance) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        balance = 0;
+    }
+}
